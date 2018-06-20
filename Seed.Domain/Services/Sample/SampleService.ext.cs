c4d0e665f9105ac27d0c9eb8fbf0c1a837a71d30
wilsonsantosnet <wilsonsantosnet@gmail.com>
2018-06-19 22:42:17 -0300

    add complexSave behavior

--- Seed.Domain/Services/Sample/SampleService.ext.cs
+++ Seed.Domain/Services/Sample/SampleService.ext.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Common.Domain.Interfaces;
 using Common.Domain.Model;
 using Seed.Domain.Entitys;
@@ -8,13 +9,22 @@ namespace Seed.Domain.Services
 {
     public class SampleService : SampleServiceBase, ISampleService
     {
+        private ISampleTagRepository _repSampleTag;
 
-        public SampleService(ISampleRepository rep, ICache cache, CurrentUser user) 
+        public SampleService(ISampleRepository rep, ISampleTagRepository repSampleTag, ICache cache, CurrentUser user) 
             : base(rep, cache, user)
         {
 
-
+            this._repSampleTag = repSampleTag;
         }
-        
+
+        public override Task<Sample> DomainOrchestration(Sample entity, Sample entityOld)
+        {
+            if (entity.AttributeBehavior == "ComplexSave")
+                this._repSampleTag.RemoveRange(entity.CollectionSampleTag);
+
+            return base.DomainOrchestration(entity, entityOld);
+        }
+
     }
 }
--- Seed.Spa.Ui/src/app/main/sample/sample.service.ts
+++ Seed.Spa.Ui/src/app/main/sample/sample.service.ts
@@ -15,81 +15,82 @@ import { MainService } from '../main.service';
 @Injectable()
 export class SampleService extends ServiceBase {
 
-    private _form : FormGroup;
+  private _form: FormGroup;
 
-    constructor(private api: ApiService<any>,private serviceFields: SampleServiceFields, private globalServiceCulture: GlobalServiceCulture, private mainService: MainService) {
+  constructor(private api: ApiService<any>, private serviceFields: SampleServiceFields, private globalServiceCulture: GlobalServiceCulture, private mainService: MainService) {
 
-        super();
-        this._form = this.serviceFields.getFormFields();
+    super();
+    this._form = this.serviceFields.getFormFields();
 
+  }
+
+  initVM(): ViewModel<any> {
+
+    return new ViewModel({
+      mostrarFiltros: false,
+      actionTitle: " Sample",
+      actionDescription: "",
+      downloadUri: GlobalService.getEndPoints().DOWNLOAD,
+      filterResult: [],
+      modelFilter: {},
+      summary: {},
+      model: {},
+      details: {},
+      infos: this.getInfos(),
+      grid: this.getInfoGrid(this.getInfos()),
+      generalInfo: this.mainService.getInfos(),
+      form: this._form,
+      masks: this.masksConfig()
+    });
+  }
+
+  getInfos() {
+    return this.serviceFields.getInfosFields();
+  }
+
+  getInfoGrid(infos: any) {
+    return super.getInfoGrid(infos)
+  }
+
+  updateCulture(culture: string = null) {
+    return this.getInfosTranslated(this.globalServiceCulture.defineCulture(culture));
+  }
+
+  updateCultureMain(culture: string = null) {
+    return this.mainService.getInfosTranslated(this.globalServiceCulture.defineCulture(culture));
+  }
+
+  getInfosTranslated(culture: string) {
+    return this.globalServiceCulture.getInfosTranslatedStrategy('Sample', culture, this.getInfos(), []);
+  }
+
+  get(filters?: any): Observable<any> {
+    return this.api.setResource('Sample').get(filters);
+  }
+
+  getDataCustom(filters?: any): Observable<any> {
+    return this.api.setResource('Sample').getDataCustom(filters);
+  }
+
+  getDataListCustom(filters?: any): Observable<any> {
+    return this.api.setResource('Sample').getDataListCustom(filters);
+  }
+
+  save(model: any): Observable<any> {
+
+    if (model.sampleId != undefined) {
+      let newModel = Object.assign(model, { attributeBehavior: "ComplexSave" })
+      return this.api.setResource('Sample').put(newModel);
     }
 
-    initVM(): ViewModel<any> {
+    return this.api.setResource('Sample').post(model);
+  }
 
-        return new ViewModel({
-            mostrarFiltros: false,
-            actionTitle: " Sample",
-            actionDescription: "",
-            downloadUri: GlobalService.getEndPoints().DOWNLOAD,
-            filterResult: [],
-            modelFilter: {},
-            summary: {},
-            model: {},
-            details: {},
-            infos: this.getInfos(),
-            grid: this.getInfoGrid(this.getInfos()),
-            generalInfo: this.mainService.getInfos(),
-            form: this._form,
-            masks: this.masksConfig()
-        });
-    }
+  delete(model: any): Observable<any> {
+    return this.api.setResource('Sample').delete(model);
+  }
 
-    getInfos() {
-        return this.serviceFields.getInfosFields();
-    }
-
-    getInfoGrid(infos : any) {
-        return super.getInfoGrid(infos)
-    }
-
-    updateCulture(culture: string = null) {
-        return this.getInfosTranslated(this.globalServiceCulture.defineCulture(culture));
-    }
-
-    updateCultureMain(culture: string = null) {
-        return this.mainService.getInfosTranslated(this.globalServiceCulture.defineCulture(culture));
-    }
-
-    getInfosTranslated(culture: string) {
-        return this.globalServiceCulture.getInfosTranslatedStrategy('Sample', culture, this.getInfos(), []);
-    }
-
-    get(filters?: any): Observable<any> {
-        return this.api.setResource('Sample').get(filters);
-    }
-
-    getDataCustom(filters?: any): Observable<any> {
-        return this.api.setResource('Sample').getDataCustom(filters);
-    }
-
-    getDataListCustom(filters?: any): Observable<any> {
-        return this.api.setResource('Sample').getDataListCustom(filters);
-    }
-
-    save(model: any): Observable<any> {
-
-        if ( model.sampleId != undefined) {
-            return this.api.setResource('Sample').put(model);
-        }
-
-        return this.api.setResource('Sample').post(model);
-    }
-
-    delete(model: any): Observable<any> {
-        return this.api.setResource('Sample').delete(model);
-    }
-    
-    export(filters?: any): Observable<any> {
-        return this.api.setResource('Sample').export(filters);
-    }
+  export(filters?: any): Observable<any> {
+    return this.api.setResource('Sample').export(filters);
+  }
 }
